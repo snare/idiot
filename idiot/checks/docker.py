0613b4d9936db12aab1f60b5b7494aeb8cfbe5ce
snare <snare@ho.ax>
2016-04-03 00:08:47 +1100

    Use shells so we don't have to hardcode paths

--- idiot/checks/docker.py
+++ idiot/checks/docker.py
@@ -15,23 +15,23 @@ class DockerCheck(CheckPlugin):
 
     def run(self):
         try:
-            output = subprocess.check_output(['/usr/local/bin/docker-machine', 'ls']).split('\n')[1:]
+            output = subprocess.check_output(['docker-machine ls'], shell=True).split('\n')[1:]
             running = [line.split()[0] for line in output if 'Running' in line]
 
             for dm in running:
                 log.debug("Enumerating docker-machine {}".format(dm))
-                env = subprocess.check_output(['docker-machine', 'env', dm])
+                env = subprocess.check_output(['docker-machine', 'env', dm], shell=True)
                 ps = subprocess.check_output('\n'.join([env, 'docker ps']), shell=True).split('\n')[1:]
                 images = [line.split()[1] for line in ps if len(line)]
                 if len(images):
                     return (False, "docker containers are running: {}".format(', '.join(images)))
                 else:
-                    return (True, "no Docker containers are running")
+                    return (True, "no docker containers are running")
         except Exception as e:
             log.exception("Failed to enumerate docker containers")
             return (False, "failed to enumerate docker containers")
 
-        return (True, "no Docker machines are running")
+        return (True, "no docker machines are running")
 
 
 if __name__ == "__main__":
